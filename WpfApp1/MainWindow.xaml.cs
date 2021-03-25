5e7d5ba545542c53d8e195a0de3c69a0c507a5a5
Ofir Shtrosberg <ofir.shtrosberg@live.biu.ac.il>
2021-03-25 14:08:19 +0200

    .

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+     
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+        }
+        // browse XML file
+        private void openXMLButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        // open FlightGear app
+        private void openFlightGear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void doublePreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void previousButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void doubleNextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        // browse CSV file button
+        private void openCSVButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
