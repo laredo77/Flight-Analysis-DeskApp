99a7ec675e6d5b3ba5d50c77464090c322a04711
Ofir Shtrosberg <ofir.shtrosberg@live.biu.ac.il>
2021-03-30 16:51:24 +0300

    30/3

---
+++ WpfApp1/SpeedClockV.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public partial class SpeedClockV
+    {
+        SpeedClockVM vm = new SpeedClockVM();
+        public void abcd(Object x)
+        {
+            vm.notify(x);
+        }
+
+    }
+   
+}
---
+++ WpfApp1/SpeedClockVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class SpeedClockVM
+    {
+
+        SpeedClockM scmodel = new SpeedClockM();
+        public void notify(Object x)
+        {
+            scmodel.printSpeed();
+        }
+
+    }
+}
