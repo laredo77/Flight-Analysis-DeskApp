8a5ba3b39f32c51883605cc2059c3b13702038ba
Ofir Shtrosberg <ofir.shtrosberg@live.biu.ac.il>
2021-04-01 01:22:55 +0300

    1/4 1:22

---
+++ WpfApp1/SpeedClockViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class SpeedClockViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+        
+        public SpeedClockViewModel()
+        {
+
+        }
+        int angle;
+        public int Angle
+        {
+            get
+            {
+                return this.angle;
+            }
+            set
+            {
+                angle = value;
+                NotifyPropertyChanged("Angle");
+            }
+        }
+        int speed;
+        public int Speed
+        {
+            get
+            {
+                return this.speed;
+            }
+            set
+            {
+                ///////////
+                if (value > 0 && value < 100)
+                {
+                    speed = value;
+                    Angle = value - 50;
+                    NotifyPropertyChanged("Speed");
+                }
+
+            }
+        }
+    }
+
+}
